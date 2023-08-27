befff90dce0f9b58cd85d333a11664bd87b6a816
Edip1 <medip642@gmail.com>
2023-08-27 19:33:17 +0300

    add memory info

---
+++ mem/mem.go
@@ -0,0 +1,37 @@
+package mem
+
+import (
+	"log"
+	"strconv"
+	"strings"
+
+	"gofretch/util"
+)
+
+func available() int {
+	line, err := util.ReadProcFile("/proc/meminfo", "MemAvailable:")
+	if err != nil {
+		log.Fatal(err)
+	}
+	memAvailableKB, _ := strconv.Atoi(strings.Fields(line[0])[1])
+	memavailable := memAvailableKB / 1024
+	return memavailable
+}
+
+func Total() int {
+	line, err := util.ReadProcFile("/proc/meminfo", "MemTotal:")
+	if err != nil {
+		log.Fatal(err)
+	}
+	memTotalKB, _ := strconv.Atoi(strings.Fields(line[0])[1])
+	memtotal := memTotalKB / 1024
+	return memtotal
+}
+
+func UsageMB() int {
+	return Total() - available()
+}
+
+func UsagePercent() int {
+	return int((float64(UsageMB()) / float64(Total())) * 100)
+}
